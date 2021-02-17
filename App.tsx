d9b9495f8d9549b5c26c25245e7ddf0679a70c42
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-17 15:12:33 -0300

    added wrapper logic of provider around the app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
+import { BlogProvider } from './src/context/BlogContext';
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator(
@@ -15,4 +17,9 @@ const navigator = createStackNavigator(
   },
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+export default () => (
+  <BlogProvider>
+    <App />
+  </BlogProvider>
+);
