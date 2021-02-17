deff27589be7fd2d8fd14b93b638ce5cfade03de
Pedro Pacheco <pacheckdev@gmail.com>
2021-02-17 15:12:57 -0300

    sample data to render and teste the app

--- src/screens/IndexScreen/index.tsx
+++ src/screens/IndexScreen/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
-import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../../context/BlogContext';
 
 const IndexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>IndexScreen</Text>
+      <Button onPress={addBlogPost} title="Add Post" />
+
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => (
+          <Text style={styles.title}>{item.title}</Text>
+        )}
+      />
     </View>
   );
 };
